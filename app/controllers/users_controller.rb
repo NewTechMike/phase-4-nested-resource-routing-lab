6ac11ba031b5bcc54611af3af8fa4514ae50eaad
Michael Harris <xero5k@yahoo.com>
2022-03-09 21:20:02 -0600

    commit tree f4afff28911f8161e11315af7a3f5d784c82e4f3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,15 @@
 class UsersController < ApplicationController
-
-  def show
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  
+def show
     user = User.find_by(id: params[:id])
     render json: user, include: :items
   end
 
+
+  private 
+
+  def render_not_found_response
+    render json: { error: "Dog house not found" }, status: 404
+  end 
 end
